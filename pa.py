c4dfa1365da5218b6236d1f29ac3ad0def5e1fa0
robinhan10211021 <1247762747@qq.com>
2022-05-05 23:27:55 +0000

    我不知道怎么提交更改

--- pa.py
+++ pa.py
@@ -1,1 +1,10 @@
+from urllib.request import urlopen
+
+url = "http://www.baidu.com"
+resp = urlopen(url)
+
+with open("mtbaidu.html", mode="w") as f:
+    f.write(resp.read().decode("utf-8"))
+print("over!")
+
 
